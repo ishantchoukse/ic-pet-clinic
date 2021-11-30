d030832e4f30cfa257d38bbfbd2af269db9d1b6a
ishantchoukse <ishantchoukse@fico.com>
2021-11-30 10:47:30 +0530

    initial commit

---
+++ src/test/java/com/ishant/icpetclinic/IcPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishant.icpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
